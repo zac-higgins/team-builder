3388a921986a732c8d4adbcf1fd31e71267d16f0
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-10-17 09:29:11 -0600

    testing random person api

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,10 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import TeamMemberForm from "./components/Form"
 import TeamMemberList from "./components/list"
+import axios from "axios";
 
 function App() {
   const [members, setMembers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://randomuser.me/api/?results=5')
+      .then(res => {
+        console.log("random people", res.data.results);
+        res.data.results.map((person) => {
+          return (
+            name: person.name.first,
+            email: person.email,
+            role: "developer"
+          )
+        })
+      })
+      .catch(err => {
+        console.log("The random people data was not returned", err);
+      })
+  })
+
 
   const addNewMember = member => {
     setMembers([...members, member]);
